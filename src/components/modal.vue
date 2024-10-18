a0b481a71388d10100dd23134034f85b9fd24782
todverse <todverse0@gmail.com>
2024-10-18 19:01:13 +0400

    more butifull kazik

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -17,13 +17,18 @@
         <section class="modal-body">
           <slot name="body">
             <div class="flex flex-wrap h-full overflow-hidden">
-                <div v-if="!Object.keys(store.state.promo).length" v-for="item in promos" class="w-1/3 h-1/3 flex items-center" @click="click(item)">
+                <div v-if="!Object.keys(store.state.promo).length" v-for="(item, index) in promos" class="w-1/3 h-1/3 flex items-center" @click="click(item, index)">
                     <div class="w-4/5 h-4/5 m-auto border-2 game-block">
 
                     </div>
                 </div>
-                <div v-else v-for="item in promos" class="w-1/3 h-1/3  flex items-center">
-                    <div class="w-4/5 h-4/5 m-auto border-2 game-block">
+                <div v-else v-for="(item, index) in promos" class="w-1/3 h-1/3  flex items-center">
+                    <div v-if="store.state.promo.index == index" class="w-4/5 h-4/5 m-auto border-2 game-block">
+                      <div class="w-full h-full flex items-center game-block_selected">
+                        <div class="m-auto font-extrabold text-5xl">{{ store.state.promo.discount }}</div>
+                      </div>
+                    </div>
+                    <div v-else class="w-4/5 h-4/5 m-auto border-2 game-block">
 
                     </div>
                 </div>
@@ -40,8 +45,12 @@
          </section>
          <footer class="modal-footer">
             <slot name="footer">
-              <component class="w-full" v-if="Object.keys(store.state.promo).length">
-                <h2 class="font-bold">Ваша скидка составляет {{ store.state.promo.discount }}<br> Промокод {{ store.state.promo.promo }}</h2>
+              <component class="w-full flex items-center" v-if="Object.keys(store.state.promo).length">
+                <h2 class="font-bold pr-2">
+                  Ваша скидка составляет 
+                  {{ store.state.promo.discount }} 
+                  Промокод 
+                  <span class="border-b-2 border-slate-950 hover:border-slate-400 hover:text-slate-400 hover:cursor-pointer" @click="copy()">{{ store.state.promo.promo }}</span></h2>
                 <button
                 type="button"
                 class="btn-green"
@@ -61,8 +70,11 @@
 
 <script setup>
 import { useStore } from 'vuex'
+import { ref } from 'vue';
 const store = useStore()
 
+let copied = ref(false)
+let openNotification = ref(false)
 
 let promos = [
     {
@@ -103,10 +115,15 @@ let promos = [
     }
 ]
 
-let click = (item) => {
+let click = (item, index) => {
     console.log(store.state.promo)
+    item.index = index
     store.commit('setPromo', item)
     console.log(store.state.promo)
+}
+let copy = () => {
+  navigator.clipboard.writeText(store.state.promo.promo)
+  alert(`Промокод ${store.state.promo.promo} успешно скопирован`)
 }
 </script>
   
@@ -173,6 +190,9 @@ let click = (item) => {
         background-position: center;
         background-size: contain;
         background-repeat: no-repeat;
+    }
+    .game-block_selected {
+      background-color: rgba(255, 255, 255, 0.8);
     }
 
     .game-block:hover {
